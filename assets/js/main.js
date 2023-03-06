00fcc5e62e3ec1dae64bbb3d0563d87dcd5104be
Miraa21 <80009478+Miraa21@users.noreply.github.com>
2023-03-05 21:57:24 -0500

    added js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 /*=============== FILTERS TABS ===============*/
-// const tabs=document.querySelectorAll('[data-target]')
-//         tabContents=document.querySelectorAll('[data-content]')
+const tabs=document.querySelectorAll('[data-target]')
+        tabContents=document.querySelectorAll('[data-content]')
 
-// tabs.forEach(tab => {
-//     tab.addEventListener('click',()=>{
-//         const target= document.querySelector(tab.dataset.target)
-//         tabContents.forEach(tc => {
-//             tc.classList.remove('filters__active')
-//         })
-//         target.classList.add('filters__active')
-//         tabs.forEach(t => {
-//             t.classList.remove('filter-tab-active')
-// });
-//     tab.classList.add('filter-tab-active')
+tabs.forEach(tab => {
+    tab.addEventListener('click',()=>{
+        const target= document.querySelector(tab.dataset.target)
+        tabContents.forEach(tc => {
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+        tabs.forEach(t => {
+            t.classList.remove('filter-tab-active')
+});
+    tab.classList.add('filter-tab-active')
  
-//     })
-//   })
+    })
+  })
 // function showHide(){
 // var x= document.querySelector("#projects")
 // if (x.style.display === "none") {
